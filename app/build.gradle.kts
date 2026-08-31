224d2095fad3147f99397eab26001f0b08bbc624
Su Yao <58110312+Indigo2233@users.noreply.github.com>
2026-08-31 17:32:31 +0800

    chore(release): bump version to 0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.indigo.mobileobservatory"
         minSdk = 26
         targetSdk = 34
-        versionCode = 15
-        versionName = "0.1.2"
+        versionCode = 27
+        versionName = "0.1.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("boolean", "STELLARIUM_ENABLED", stellariumNonCommercial.toString())
 
