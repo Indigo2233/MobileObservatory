01289ec1e7d5de0e5d2ed2babf4bff72af285b06
Su Yao <58110312+Indigo2233@users.noreply.github.com>
2026-09-23 14:26:29 +0800

    chore(release): bump consumer version to 1.0.6 (40)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.indigo.mobileobservatory"
         minSdk = 26
         targetSdk = 34
-        versionCode = 39
-        versionName = "1.0.5"
+        versionCode = 40
+        versionName = "1.0.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("boolean", "STELLARIUM_ENABLED", stellariumNonCommercial.toString())
 
